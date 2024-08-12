59478cc6ad4d31b394cfa93ac3987a5ae044119a
Ritish007-lang <ritish.sachdeva@flipkart.com>
2024-08-12 23:43:35 +0530

    Added Javadoc Comments for utils folder.

--- CRS-FLIPKART-GROUP-H-JAVA-POS/src/com/flipkart/utils/DBQueries.java
+++ CRS-FLIPKART-GROUP-H-JAVA-POS/src/com/flipkart/utils/DBQueries.java
@@ -1,81 +1,120 @@
 package com.flipkart.utils;
 
+/**
+ * This class contains SQL queries used in the application.
+ * The queries are categorized based on their functionalities, such as user operations, course operations, and student operations.
+ */
 public class DBQueries {
-	public static final String GET_USER_USERNAME = "SELECT * FROM user WHERE username = ?";
-	
-	public static final String GET_USER_ID = "SELECT * FROM user WHERE userID = ?";
+    
+    /** Query to get a user by their username. */
+    public static final String GET_USER_USERNAME = "SELECT * FROM user WHERE username = ?";
+    
+    /** Query to get a user by their user ID. */
+    public static final String GET_USER_ID = "SELECT * FROM user WHERE userID = ?";
 
-	public static final String GET_STUDENT_USERID = "SELECT * FROM student WHERE userID = ?";
-	
-	public static final String GET_PROFESSOR_USERID = "SELECT * FROM professor WHERE userID = ?";
+    /** Query to get a student by their user ID. */
+    public static final String GET_STUDENT_USERID = "SELECT * FROM student WHERE userID = ?";
+    
+    /** Query to get a professor by their user ID. */
+    public static final String GET_PROFESSOR_USERID = "SELECT * FROM professor WHERE userID = ?";
 
-	public static final String VERIFY_USERNAME= "SELECT * FROM user where username = ?";
+    /** Query to verify if a username already exists. */
+    public static final String VERIFY_USERNAME= "SELECT * FROM user where username = ?";
+    
+    /** Query to fetch all user IDs. */
+    public static final String FETCH_IDS= "SELECT userID FROM user";
+    
+    /** Query to fetch all roll numbers of students. */
+    public static final String FETCH_ROLLNUMS= "SELECT rollNum FROM student";
 
-	public static final String FETCH_IDS= "SELECT userID FROM user";
-	
-	public static final String FETCH_ROLLNUMS= "SELECT rollNum FROM student";
+    /** Query to add a new user to the database. */
+    public static final String ADD_USER = "INSERT INTO user(userID, name, role, contact, email, password, username) VALUES (?, ?, ?, ?, ?, ?, ?)";
+    
+    /** Query to add a new student to the database. */
+    public static final String ADD_STUDENT = "INSERT INTO student(userID, branch, rollNum, approved) VALUES (?, ?, ?, ?)";
 
-	public static final String ADD_USER = "insert into user(userID,name,role,contact,email,password,username) values (?,?,?,?,?,?,?)";
-	
-	public static final String ADD_STUDENT = "insert into student(userID,branch,rollNum,approved) values (?,?,?,?)";
+    /** Query to update a user's password. */
+    public static final String UPDATE_PASSWORD = "UPDATE user SET password = ? WHERE username = ? AND password = ?";
+    
+    /** Query to fetch all courses from the catalog. */
+    public static final String FETCH_COURSES = "SELECT * FROM course";
+    
+    /** Query to add a new course to the catalog. */
+    public static final String ADD_COURSE = "INSERT INTO catalog VALUES (?, ?, NULL, ?, ?)";
+    
+    /** Query to remove a course from the catalog. */
+    public static final String REMOVE_COURSE = "DELETE FROM catalog WHERE courseID = ?";
+    
+    /** Query to remove a user from the database. */
+    public static final String REMOVE_USER = "DELETE FROM user WHERE userID = ?";
+    
+    /** Query to remove a professor from the database. */
+    public static final String REMOVE_PROFESSOR = "DELETE FROM professor WHERE userID = ?";
+    
+    /** Query to add a new professor to the database. */
+    public static final String ADD_PROFESSOR = "INSERT INTO professor VALUES (?, ?, ?)";
+    
+    /** Query to approve a student's registration. */
+    public static final String APPROVE_REGISTRATION = "UPDATE student SET approved = 1 WHERE userID = ?";
+    
+    /** Query to view the professor assigned to a course. */
+    public static final String VIEW_VACANT_COURSE = "SELECT courseProf FROM catalog WHERE courseID = ?";
+    
+    /** Query to select a professor for a course. */
+    public static final String SELECT_PROF_COURSE = "UPDATE catalog SET courseProf = ? WHERE courseID = ?";
+    
+    /** Query to view the list of students enrolled in a course. */
+    public static final String VIEW_STUDENT_LIST = "SELECT * FROM student JOIN registeredCourse ON student.userID = registeredCourse.studentID JOIN user ON student.userID = user.userID WHERE registeredCourse.courseID = ?"; 
+    
+    /** Query to set a grade for a student in a course. */
+    public static final String SET_GRADE = "INSERT INTO reportCard(studentID, courseID, grade) VALUES (?, ?, ?)";
+    
+    /** Query to view the course catalog. */
+    public static final String VIEW_COURSE_CATALOG = "SELECT * FROM crsapp.catalog";
+    
+    /** Query to get grades for a specific student. */
+    public static final String GET_GRADES = "SELECT * FROM reportCard WHERE studentID = ?";
+    
+    /** Query to get the courses enrolled by a student. */
+    public static final String GET_COURSE_ENROLLED = "SELECT catalog.courseID, catalog.courseName, catalog.courseProf, catalog.seats FROM student JOIN registeredCourse ON student.userID = registeredCourse.studentID JOIN catalog ON registeredCourse.courseID = catalog.courseID WHERE student.userID = ?";
+    
+    /** Query to get billing information for a student. */
+    public static final String GET_BILLING_INFO = "SELECT * FROM billing WHERE studentID = ?";
+    
+    /** Query to get a course by its ID. */
+    public static final String GET_COURSE = "SELECT * FROM catalog WHERE courseID = ?";
 
-	public static final String UPDATE_PASSWORD = "UPDATE user SET password = ? WHERE username = ? and password = ?";
-	
-	public static final String FETCH_COURSES = "select * from course";
-	
-	public static final String ADD_COURSE = "INSERT INTO catalog VALUES (?,?,null,?,?)";
-	
-	public static final String REMOVE_COURSE = "delete from catalog where courseID = ?";
-	
-	public static final String REMOVE_USER = "delete from user where userID = ?";
-	
-	public static final String REMOVE_PROFESSOR = "delete from professor where userID=?";
-	
-	public static final String ADD_PROFESSOR = "insert into professor values (?,?,?)";
-	
-	public static final String APPROVE_REGISTRATION = "UPDATE student SET approved = 1 where userID = ?";
-	
-	public static final String VIEW_VACANT_COURSE = "SELECT courseProf from catalog WHERE courseID = ?";
-	
-	public static final String SELECT_PROF_COURSE = "UPDATE catalog SET courseProf = ? WHERE courseID = ?";
-	
-	public static final String VIEW_STUDENT_LIST = "SELECT * FROM student JOIN registeredCourse ON student.userID = registeredCourse.studentID JOIN user ON student.userID = user.userID WHERE registeredCourse.courseID = ?"; 
-	
-	public static final String SET_GRADE = "INSERT INTO reportCard(studentID, courseID, grade) values(?, ?, ?)";
-	
-	public static final String VIEW_COURSE_CATALOG = "SELECT * FROM crsapp.catalog";
-	
-	public static final String GET_GRADES = "SELECT * from reportCard WHERE studentID = ?";
-	
-	public static final String GET_COURSE_ENROLLED="SELECT catalog.courseID, catalog.courseName, catalog.courseProf, catalog.seats FROM student JOIN registeredCourse ON student.userID = registeredCourse.studentID JOIN catalog ON registeredCourse.courseID = catalog.courseID WHERE student.userID = ?";
-	
-	public static final String GET_BILLING_INFO="SELECT * from billing WHERE studentID = ?";
-	
-	public static final String GET_COURSE="SELECT * FROM catalog WHERE courseID = ?";
+    /** Query to decrement the number of seats available for a course. */
+    public static final String DECR_SEATS = "UPDATE catalog SET seats = ? WHERE courseID = ?";
+    
+    /** Query to register a student for a course. */
+    public static final String REGISTER_COURSE = "INSERT INTO registeredCourse(studentID, courseID, date) VALUES (?, ?, ?)";
+    
+    /** Query to add billing information for a student. */
+    public static final String ADD_BILLING = "INSERT INTO billing(studentID, billingID, billAmount, status) VALUES (?, ?, ?, ?)";
+    
+    /** Query to fetch all billing IDs. */
+    public static final String FETCH_BILLINGS = "SELECT billingID FROM billing";
 
-	public static final String DECR_SEATS = "UPDATE catalog SET seats = ? where courseID = ?";
-	
-	public static final String REGISTER_COURSE = "INSERT INTO registeredCourse(studentID,courseID,date) VALUES (?,?,?)";
-	
-	public static final String ADD_BILLING = "INSERT INTO billing(studentID,billingID,billAmount,status) VALUES (?,?,?,?)";
-	
-	public static final String FETCH_BILLINGS= "SELECT billingID FROM billing";
+    /** Query to get the professor assigned to a specific course. */
+    public static final String GET_PROFESSOR_COURSEID = "SELECT courseProf FROM catalog WHERE courseID = ?";
 
-	public static final String GET_PROFESSOR_COURSEID="SELECT courseProf FROM catalog WHERE courseID=?";
+    /** Query to check if a student is enrolled in a course. */
+    public static final String CHECK_STUDENT_COURSE = "SELECT studentID FROM registeredCourse WHERE courseID = ? AND studentID = ?";
+    
+    /** Query to view courses offered by a specific professor. */
+    public static final String VIEW_COURSE_ENROLLED = "SELECT * FROM catalog WHERE courseProf = ?";
+    
+    /** Query to update billing information. */
+    public static final String UPDATE_BILLING_INFO = "UPDATE billing SET billamt = ?, status = ?, transactionID = ? WHERE billingID = ?";
+    
+    /** Query to view unapproved students. */
+    public static final String VIEW_UNAPPROVED_STUDENTS = 
+        "SELECT user.userID, user.name, user.contact, user.email, student.branch, student.rollNum, student.approved, user.password " +
+        "FROM student " +
+        "JOIN user ON student.userID = user.userID " +
+        "WHERE student.approved = 0";
 
-	public static final String CHECK_STUDENT_COURSE="SELECT studentID FROM registeredCourse WHERE courseID=? AND studentID=? ";
-
-	public static final String VIEW_COURSE_ENROLLED="SELECT * FROM catalog WHERE courseProf = ?";
-	
-	public static final String UPDATE_BILLING_INFO = "UPDATE billing SET billamt = ?, status = ?, transactionID = ? WHERE billingID = ?";
-	
-	public static final String VIEW_UNAPPROVED_STUDENTS =
-		    "SELECT user.userID, user.name, user.contact, user.email, student.branch, student.rollNum, student.approved, user.password " +
-		    "FROM student " +
-		    "JOIN user ON student.userID = user.userID " +
-		    "WHERE student.approved = 0";
-
-	
-	public static final String VIEW_PROF_LIST = "SELECT * FROM professor JOIN user ON professor.userID=user.userID";
-	
+    /** Query to view the list of professors. */
+    public static final String VIEW_PROF_LIST = "SELECT * FROM professor JOIN user ON professor.userID = user.userID";
 }
